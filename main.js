9c5fc1cfeb78139fbb1e0a76134ac51c87d9bd43
Tarachan Rana <103596256+Rtarachan02@users.noreply.github.com>
2024-04-13 13:24:38 +0530

    Initial Part

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+    strings : ["Frontend Developer","Backend Developer"," Fullstack Web Developer","YouTuber"],
+    typeSpeed:50,
+    backSpeed:20,
+    backDelay:500,
+    loop:true
+})
